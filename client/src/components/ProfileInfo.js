1f08e2ecf72059ab0d1cbf316dbec2c0e99cea05
Anderson Luan <anderson@Andersons-MacBook-Pro.local>
2020-01-25 23:46:36 -0500

    Styled profile

--- client/src/components/ProfileInfo.js
+++ client/src/components/ProfileInfo.js
@@ -1,44 +1,20 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
 const ProfileInfo = (props) => {
     return(
         <div>
-            <img alt="avatar" src ={props.pp}/>
-            <form>
-              <label>
-                <p>First Name</p>
-                <TextField
-                    id="filled-read-only-input"
-                    defaultValue={props.fName}
-                    InputProps={{readOnly: true,}}/>
+        <Grid container spacing={2} justify="center">
+            <Grid item md={6} style={{textAlign:'right'}}>
+                <p>{props.title}</p>
+            </Grid>
+            <Grid item md={6}style={{textAlign:'left'}}>
 
-                <p>Last Name</p>
-                <TextField
-                    id="filled-read-only-input"
-                    defaultValue={props.lName}
-                    InputProps={{readOnly: true,}}/>
-
-                <p>Phone Number</p>
-                <TextField
-                    id="filled-read-only-input"
-                    defaultValue={props.phone}
-                    InputProps={{readOnly: true,}}/>
-
-                <p>Email</p>
-                <TextField
-                    id="filled-read-only-input"
-                    defaultValue={props.email}
-                    InputProps={{readOnly: true,}}/>
-                    
-                <p>Password</p>
-                <TextField
-                    id="filled-read-only-input"
-                    defaultValue={props.password}
-                    InputProps={{readOnly: true,}}/>
-
-              </label>
-            </form>
-            </div>
+                    <p>{props.content}</p>
+            </Grid>
+        </Grid>
+        </div>
     );
 };
 
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,24 +1,59 @@
 import React, { Component } from 'react'
 import faker from 'faker';
 import ProfileInfo from '../components/ProfileInfo';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+
+
 
 export class Profile extends Component {
     render() {
         return (
 
+            <Grid container spacing={1}>
+            <Grid item md={2}/>
 
-            <div style= {{display:'flex',justifyContent:'center', alignItems:'center'}}>
+
+            <Grid item md={8}>
                 <div>
-                    <h1>Profile</h1>
-                    <ProfileInfo
-                    pp={faker.image.avatar()}
-                    fName="Daddy"
-                    lName="Wayne"
-                    phone="416-629-6921"
-                    email="DaddyWayne@gmail.com"
-                    password="***********"/>
+                    <div style={{fontSize:20}}>
+
+                            <h1 style={{textAlign:'center'}}>Profile</h1>
+                            <Grid container spacing={2}>
+                            <Grid item md={6} style={{textAlign:'right'}}>
+                            <form>
+                                <ProfileInfo title="First Name:" content="Daddy"/>
+                                <ProfileInfo title="Last Name:" content="Wayne"/>
+                                <ProfileInfo title="Phone Number:" content="416-629-6921"/>
+                                <ProfileInfo title="Email:" content="DaddyWayne@gmail.com"/>
+                                <ProfileInfo title="Password:" content="***********"/>
+                            </form>
+                            </Grid>
+                            <Grid item md={6} style={{textAlign:'center'}}>
+                                <img alt="avatar" src ={faker.image.avatar()} style={{width:"50%"}}/>
+                            </Grid>
+                            </Grid>
+                            <Grid container spacing={2}>
+                                <Grid item md={3} style={{textAlign:'right'}}>
+                               <p>Favourites: </p>
+                            </Grid>
+                            <Grid item md={3}>
+                                <img alt="avatar" src ="https://wba-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/CoCo-Logo--1024x1024.jpg" style={{maxWidth:"70%",maxHeight:"70%"}}/>
+                            </Grid>
+                            <Grid item md={3}>
+                                <img alt="avatar" src ="https://wba-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/CoCo-Logo--1024x1024.jpg" style={{maxWidth:"70%",maxHeight:"70%"}}/>
+                            </Grid>
+                            <Grid item md={3}>
+                                <img alt="avatar" src ="https://wba-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/CoCo-Logo--1024x1024.jpg" style={{maxWidth:"70%",maxHeight:"70%"}}/>
+                            </Grid>
+                            </Grid>
+
+                    </div>
                 </div>
-            </div>
+            </Grid>
+
+            <Grid item md={2}/>
+            </Grid>
         );
     }
 }
